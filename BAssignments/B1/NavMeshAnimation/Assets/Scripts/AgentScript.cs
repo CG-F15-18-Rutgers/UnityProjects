9a0149bd0400a794e1a9fee3fee0458f3452a89c
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2015-10-14 00:50:32 -0400

    Add basic scene with navmesh and navmeshagent

---
+++ BAssignments/B1/NavMeshAnimation/Assets/Scripts/AgentScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentScript : MonoBehaviour {
+
+    public float thrust = 500;
+    public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();   
+	}
+	
+    // Update is called every frame
+    void Update ()
+    {
+        float moveHorizontal = Input.GetKey(left) ? -1 : 0;
+        moveHorizontal = Input.GetKey(right) ? 1 : moveHorizontal;
+        float moveVertical = Input.GetKey(up) ? 1 : 0;
+        moveVertical = Input.GetKey(down) ? -1 : moveVertical;
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rigidBody.AddForce(Time.deltaTime * movement * thrust);
+    }
+
+	// FixedUpdate is called every fixed framerate frame
+	void FixedUpdate () {
+        
+    }
+
+    private Rigidbody rigidBody;
+}
---
+++ BAssignments/B1/NavMeshAnimation/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        offset = this.transform.position - player.transform.position;
+	}
+	
+	void LateUpdate () {
+        this.transform.position = player.transform.position + offset;
+	}
+
+    private Vector3 offset;
+}
