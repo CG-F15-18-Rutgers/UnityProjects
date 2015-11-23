2c823bdc32133e0af1c3fc4d76bc32db18f27f2a
Ananth Rao <ananthamapod@gmail.com>
2015-11-22 20:07:23 -0500

    Add Multiple cameras and add walls

---
+++ BAssignments/B2/Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject[] cameras;
+    private int currCam = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        cameras = GameObject.FindGameObjectsWithTag("Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            currCam--;
+        } else if (Input.GetKeyDown(KeyCode.X)) {
+            currCam++;
+        }
+
+        Debug.Log(cameras.Length);
+
+        int index = Mathf.Abs(currCam % cameras.Length);
+        Debug.Log(index);
+
+        foreach (GameObject camera in cameras) {
+            camera.SetActive(false);
+        }
+        cameras[index].SetActive(true);
+    }
+}
